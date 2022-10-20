00ef8d014c0fe477467d84bf321af47fb2c4ab70
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-20 09:55:44 +0500

    asd

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    s=n//1000%10
+    d=n//100%10
+    f=n//10%10
+    g=n%10
+    if a>s and a>d and a>f and a>g:
+        return a
+    if s>a and s>d and s>f and s>g:
+        return s 
+    if d>a and d>s and d>f and d>g:
+        return d 
+    if f>a and f>s and f>d and f>g:
+        return f 
+    if g>a and g>s and g>d and g>f:
+        return g
+print(main(54281))
