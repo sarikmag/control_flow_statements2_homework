40f8ec6d073391ef4425a244423a9f740ab8196e
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-20 09:55:34 +0500

    asd

--- if02.py
+++ if02.py
@@ -8,4 +8,9 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a<b and a<c:
+        return a
+    if b<a and b<c:
+        return b
+    return c
+print(main(-5, -3, -1))
